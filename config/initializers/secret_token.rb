3b5c66592f49d0d1e4b8524e6d518ce3f33e2fe1
George Thomas <georgetamayothomas@gmail.com>
2013-12-25 15:14:42 +0000

    Initial commit
    Working Devise. No Omniauth or Google login yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleAuthTest::Application.config.secret_key_base = '8b7e6e8c3f4135f8126cb95a92b9e1bfb58d9016cf0a495aa28ed6a0d4e26ebd2c18782ec2b1d752eedc637a2d4c039efce42e1890fffb33c0d93cfe9df96de1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoogleAuthTest::Application.config.session_store :cookie_store, key: '_GoogleAuthTest_session'
